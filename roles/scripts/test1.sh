7e544c143ba5f8ec5adf7605dd118feaf69cab1a
pawan79797979 <pawan.pandey79797979@gmail.com>
2018-02-13 11:32:19 +0000

    s c

---
+++ roles/scripts/test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#var1=$1
+#var2=$2
+#var3=$3
+
+touch /home/testing.txt
+echo  "This is first argument  $1"  >> /home/testing.txt
+echo "This is third argument $3"  >> /home/testing.txt
